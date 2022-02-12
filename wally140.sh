b14b608b97ac2c91f6b6164e2edb6428b7ac9556
Ian Brown <igb@hccp.org>
2022-02-12 08:42:11 -0800

    only edits as much as needed

--- wally140.sh
+++ wally140.sh
@@ -1,9 +1,37 @@
+IFS=""
+
 if [ "$#" -ne 1 ]; then
     wordle=`pbpaste`;
 else
     wordle=`cat $1`
 fi
     
+filters=(
+    "s/Line //;"
+    "s/and/\&/g;"
+    "s/in the wrong/in wrong/g;"
+    "s/\.//;"
+    "s/correct/right/g;"
+    "s/!//;"
+    "s/://;"
+    "s/th / /g;"
+    "s/th,/,/g;"
+    "s/st / /g;"
+    "s/st,/,/g;"
+    "s/,//g;")
 
-  wordle=`echo "$wordle" | sed 's/Line //; s/and/\&/g; s/in the wrong/in wrong/g; s/\.//; s/correct/right/g; s/!//; s/://; s/th / /g; s/th,/,/g; s/st / /g; s/st,/,/g;'`
-    echo "$wordle" | wc -c 
+
+for t in ${filters[@]}; do
+    if [ `echo $wordle | wc -c` -gt 140 ]; then
+	wordle=`echo "$wordle" | sed $t`
+    fi
+done
+
+if [ `echo $wordle | wc -c` -gt 140 ]; then
+    echo "Could not express your Wordle text in 140 characters or less. :-(" 
+else
+    score=`echo $wordle | wc -c | xargs`
+    echo "$wordle" | pbcopy
+    echo "Rewrote your Wordle text in $score characters and copied it to the clipboard."
+fi
+
