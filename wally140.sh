f315380957bdfe1c6ae253e038d86a0d23a802ab
Ian Brown <igb@hccp.org>
2022-02-12 07:50:28 -0800

    still overly greedy, minor refactoring

--- wally140.sh
+++ wally140.sh
@@ -1,6 +1,9 @@
 if [ "$#" -ne 1 ]; then
-    foo=`pbpaste | sed 's/Line //; s/and/\&/g; s/in the wrong/in wrong/g; s/\.//; s/correct/right/g; s/!//; s/://; s/th / /g; s/th,/,/g; s/st / /g; s/st,/,/g;'`
-    echo "$foo" | wc -c 
+    wordle=`pbpaste`;
 else
-    sed -i 's/search_string/replace_string/' $1
+    wordle=`cat $1`
 fi
+    
+
+  wordle=`echo "$wordle" | sed 's/Line //; s/and/\&/g; s/in the wrong/in wrong/g; s/\.//; s/correct/right/g; s/!//; s/://; s/th / /g; s/th,/,/g; s/st / /g; s/st,/,/g;'`
+    echo "$wordle" | wc -c 
